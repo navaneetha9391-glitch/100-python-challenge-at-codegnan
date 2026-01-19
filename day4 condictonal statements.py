be53be6b33f0f1f5f4c6cb4520a7b0782a8180db
charagonda Navaneetha <navaneetha9391@gmail.com>
2026-01-19 15:51:21 +0530

    Add files via upload
    condictonal  statements

---
+++ day4 condictonal statements.py
@@ -0,0 +1,30 @@
+# if number is zero print like"Enteted number is zero"
+n=int(input())
+if n==0:
+   print("Enteted number is zero")
+print("program done")  
+
+
+
+#cheke if  number is zero or not
+num=int (input ("Enter a number:"))
+if num==0:
+    print("Number is zero")
+else:
+
+
+    print("Number is not zero")
+num=int (input ("Enter a number:"))
+if num!==0:
+    print("Number is zero")
+else:
+    print("Number is not zero")
+
+
+
+ #check if number is even or odd
+    num=int (input ("Enter a number:"))
+if num% 2==0:
+    print("Number is even")
+else:
+    print("Number is odd")
