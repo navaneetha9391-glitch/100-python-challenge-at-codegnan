36fb915028ca2952afe8b1557d092f940ba50c9c
charagonda Navaneetha <navaneetha9391@gmail.com>
2026-01-19 15:49:17 +0530

    Add files via upload
    datatypes and python featares

---
+++ day2.py
@@ -0,0 +1,15 @@
+ #datatypes,variables and I0 functions
+name="Navaneetha"#string type
+age=23#integer
+weight=57.5#float datatype
+list1=[1,2,3,1,2,'HI',2,4,(10,20)]#list of items
+tuple1=(10,20,30,'WELCOM','HELLO')#tuple of items
+cards={'A',2,3,4,5,6,7,8,9,10,'J','Q','K',2,3,4,}#pack of cards in a set datatype
+student_marks={'MATHS':90,"telugu":85,"ENGLISH":75}#dictionary datatype
+
+print("my name is :", name )
+print("my age is: ",age)
+print("my weight is:",weight)
+print("list of items:",list1)
+print("pack of card values:",cards)
+print("student subject wise marks:",student_marks)
